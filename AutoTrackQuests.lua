4d58e3e41b8df3dbda4e921ef405ba5497f0583b
Anthony M <tonylemur@gmail.com>
2019-08-31 11:48:36 -0400

    No longer track untrackable quests, and untrack oldest quests if we're at the tracking limit

--- AutoTrackQuests.lua
+++ AutoTrackQuests.lua
@@ -3,7 +3,18 @@ AutoTrackQuests.Frame = CreateFrame("Frame")
 AutoTrackQuests.Frame:SetScript("OnEvent", function(self, event, ...)
 	if event == "QUEST_ACCEPTED" then
 		local qindex = ...
-		AddQuestWatch(qindex)
+		local num_objectives = GetNumQuestLeaderBoards(qindex)
+		
+		-- Don't track an untrackable quest (no objectives)
+		if num_objectives > 0 then
+			-- If we're tracking 5 or more quests, keep untracking the oldest ones until we're at 4 so we can track the new one
+			while GetNumQuestWatches() >= 5 do
+				RemoveQuestWatch(GetQuestIndexForWatch(1))
+				QuestWatch_Update()
+			end
+			
+			AddQuestWatch(qindex)
+		end
 	end
 end)
 AutoTrackQuests.Frame:RegisterEvent("QUEST_ACCEPTED")
